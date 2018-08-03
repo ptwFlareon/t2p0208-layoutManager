e7e26a87f13e47ddefbc7e16d9902de2b3784af5
Thallys Eduardo Calory <thallyscalory@hotmail.com>
2018-08-02 22:22:05 -0300

    commit

--- src/br/com/adrianob/layout/LayoutManagers.java
+++ src/br/com/adrianob/layout/LayoutManagers.java
@@ -1,5 +1,8 @@
 package br.com.adrianob.layout;
 
+import br.com.thallyscalory.TelaThallysBorderFactory;
+import br.com.thallyscalory.TelaThallysFactory;
+import br.com.thallyscalory.TelaThallysGridFactory;
 import javax.swing.JFrame;
 
 /**
@@ -14,6 +17,9 @@ public class LayoutManagers {
     public static void main(String[] args) {
         TelaFactory[] minhasFabricasDeTela = new TelaFactory[4];
         minhasFabricasDeTela[0] = new TelaAdrianoFactory();
+        minhasFabricasDeTela[1] = new TelaThallysFactory();
+        minhasFabricasDeTela[2] = new TelaThallysBorderFactory();
+        minhasFabricasDeTela[3] = new TelaThallysGridFactory();
         //minhasFabricasDeTela[1] = new TelaAdrianoFactory(); //Instanciar sua fabrica de tela 1 aqui
         //minhasFabricasDeTela[2] = new TelaAdrianoFactory(); //Instanciar sua fabrica de tela 2 aqui
         //minhasFabricasDeTela[3] = new TelaAdrianoFactory(); //Instanciar sua fabrica de tela 3 aqui
