4a1da96fa68ea2017b740f914edcf09df1761f95
Marlenis <marlonalexandrematos@gmail.com>
2018-08-02 22:24:39 -0300

    implementado

--- src/br/com/adrianob/layout/LayoutManagers.java
+++ src/br/com/adrianob/layout/LayoutManagers.java
@@ -1,5 +1,6 @@
 package br.com.adrianob.layout;
 
+import br.com.marlenis.Tela2Factory;
 import javax.swing.JFrame;
 
 /**
@@ -14,9 +15,9 @@ public class LayoutManagers {
     public static void main(String[] args) {
         TelaFactory[] minhasFabricasDeTela = new TelaFactory[4];
         minhasFabricasDeTela[0] = new TelaAdrianoFactory();
-        //minhasFabricasDeTela[1] = new TelaAdrianoFactory(); //Instanciar sua fabrica de tela 1 aqui
-        //minhasFabricasDeTela[2] = new TelaAdrianoFactory(); //Instanciar sua fabrica de tela 2 aqui
-        //minhasFabricasDeTela[3] = new TelaAdrianoFactory(); //Instanciar sua fabrica de tela 3 aqui
+        minhasFabricasDeTela[1] = new br.com.marlenis.TelaFactory(); //Instanciar sua fabrica de tela 1 aqui
+        minhasFabricasDeTela[2] = new br.com.marlenis.Tela1Factory(); //Instanciar sua fabrica de tela 2 aqui
+        minhasFabricasDeTela[3] = new Tela2Factory(); //Instanciar sua fabrica de tela 3 aqui
         new Sample(minhasFabricasDeTela).setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
